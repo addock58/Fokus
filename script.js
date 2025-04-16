33e799b13687576c6d5c0a120b1c9e86935954b2
addock58 <pedromonteiro0508@gmail.com>
2025-04-16 13:36:21 -0300

    feat(modos): adiciona função de modos

---
+++ script.js
@@ -0,0 +1,44 @@
+const html = document.querySelector('html')
+const banner = document.querySelector('.app__image')
+
+//Adicionando o evento de clique no botão de foco, curto e longo
+document.querySelectorAll('.app__card-button').forEach(btn => {
+    btn.addEventListener('click', () => {
+        alterContext(btn.getAttribute('data-contexto'))
+        activeBtn(btn)
+    })
+})
+
+function alterContext (contexto) {
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `./assets/img/${contexto}.png`)
+
+    const titulo = document.querySelector('.app__title')
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>`
+            
+            break;
+        case 'descanso-curto':
+            titulo.innerHTML = `Que tal dar uma respirada?<br>
+                <strong class="app__title-strong">Faça uma pausa curta!</strong>`
+            
+            break;
+        case 'descanso-longo':
+            titulo.innerHTML = `Hora de voltar à superfície.<br>
+                <strong class="app__title-strong">Faça uma pausa longa.</strong>`
+    
+        default:
+            break;
+    }
+}
+
+function activeBtn (button) {
+    document.querySelectorAll('.app__card-button').forEach(btn => {
+        btn.classList.remove('active')
+    })
+
+    button.classList.add('active')
+}
+
